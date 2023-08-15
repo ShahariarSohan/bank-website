f0e21b16b0f985f4f2cbfbc8f3111f74d623e0b5
Shahariar Sohan <Sohanshahariar4@gmail.com>
2023-08-15 06:43:00 +0600

    deposit added

---
+++ index1.js
@@ -0,0 +1,5 @@
+document.getElementById('deposit-btn').addEventListener('click',function(){
+   const depositString = document.getElementById('deposit-amount')
+   const depositNumber = parseFloat(depositString.value);
+   
+})
