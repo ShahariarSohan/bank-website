a9906af2cee1007fba44eda45f59414114899372
Shahariar Sohan <Sohanshahariar4@gmail.com>
2023-08-15 09:47:09 +0600

    bank simple website completed

--- withdraw.js
+++ withdraw.js
@@ -10,6 +10,11 @@ document.getElementById('withdraw-btn').addEventListener('click',function(){
 // total balance after withdraw   
    const balanceDisplayString =document.getElementById('balance').innerText
    const balanceDisplayNumber = parseFloat(balanceDisplayString);
+//if anyone withdraw amount more than total balance
+   if(withdrawAmountNumber>balanceDisplayNumber){
+     alert('Safwan ato tiya nai tor tun gorot ja')
+     return;
+   }
    const totalBalance = balanceDisplayNumber-withdrawAmountNumber;
    document.getElementById('balance').innerText=totalBalance;
    
